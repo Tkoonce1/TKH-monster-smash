1f4de0f0337c6f5cf2f1f12f215a15c8f6480f85
Tkoonce1 <112811613+Tkoonce1@users.noreply.github.com>
2022-11-21 16:52:49 -0500

    Adding monster code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //global variables, can be accessed by all functions
 
-  //declare a variable named playerName that stores the value the player enters from a prompt
+const playerName=prompt("entername")
 
-  //declare a variable named playerHealth and set it equal to the number value 15
+let playerHealth=15
 
-  //assign a name of a monster (ex 'Werewolf') as a string to a variable named monsterName
+const monsterName="Hawk"
 
-  //declare a variable named monsterHealth and set it equal to the number value 15
+let monsterHealth=15
 
 //random integer function 
 //see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -18,17 +18,19 @@ function randomNum(min, max) {
 }
 
 function playerAttack(){
-//use randomNum to generate attack points value between 1 - 5 and save the value to a variable named playerAttackPoints
+let playerAttackPoints=randomNum(1,6)
 
-//subtract playerAttackPoints from monsterHealth and update the monsterHealth variable
+monsterHealth=monsterHealth-playerAttackPoints
 
   //use an alert with string template literals to tell the player: 
-  // 1. player attacked monster 
+  // 1. player attacked monster
   // 2. how much damage the player did 
   // 3. how much health the monster has 
+  alert(`${playerName} attack ${monsterName}.${playername} did ${playerAttackPoints} damage to ${monsterName}.${monsterName} has ${monsterHealth}.`)
 }
 
 function monsterAttack(){
+let monsterAttack=randomNum(1,5)
   //use randomNum to generate attack points value between 1 - 5 and save the value to a variable named monsterAttackPoints
 
   //subtract monsterAttackPoints from playerHealth and update the playerHealth variable 
@@ -37,16 +39,33 @@ function monsterAttack(){
   // 1. monster attacked player 
   // 2. how much damage the monster did 
   // 3. how much health the player has 
+  alert(`${monsterName} attack ${playerName}.${monsterName} did ${monsterAttckPoints}
+  damage to ${playerName}.${playerName} has ${playerHealth}.`)
 }
 
 function playRound() {
-  //use randomNum to return either 0 or 1
+  let any=randomNum(0,2)
   
-  //0 = player goes first, 1 = monster goes first
+  //0 = pl
   
-  //use if/else to determine who goes first
-  
-  //if player goes first, run playerAttack, then if monsterHealth > 0, run monsterAttack
+  if(any===0){
+    playerAttack()
+    if(monsterHealth>0){
+      monsterAttack()
+    }
+
+  }
+  else{
+    monsterAttack()
+    if(playerHealth>0){
+      playerAttack()
+    }
+  }
+  monsterAttack()
+    if(playerHealth>0){
+      playerAttack()
+    }
+  //if player goes first, run playerAttack, then if monsterHealth > 0, run 
 
   //if monster goes first, run monsterAttack, then if playerHealth > 0, run playerAttack 
 }
